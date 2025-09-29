71c072810cad9c3668361936777f4f9edd5a79d1
Derek Petersen <derek@djp.fyi>
2025-09-29 16:05:58 -0700

    remove fatal

--- cmd/refuss/main.go
+++ cmd/refuss/main.go
@@ -18,20 +18,34 @@ const (
 )
 
 func main() {
-	cfg := loadcfg(replaceHome(HACK_CONFIG))
+	cfg, err := loadcfg(replaceHome(HACK_CONFIG))
+	if err != nil {
+		log.Printf("Failed to load config: %v", err)
+		os.Exit(1)
+	}
+	
+	rep, err := createRep(cfg)
+	if err != nil {
+		log.Printf("Failed to create representation: %v", err)
+		os.Exit(1)
+	}
+	
 	opts := &fs.Options{}
 	raw := fs.NewNodeFS(&rss.FSRSS{
-		InternalRep: createRep(cfg),
+		InternalRep: rep,
 	}, opts)
 	server, err := fuse.NewServer(raw, replaceHome(HACK_DIR), &opts.MountOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Failed to create FUSE server: %v", err)
+		os.Exit(1)
 	}
 	go server.Serve()
 	if err := server.WaitMount(); err != nil {
-		log.Fatal(err)
+		log.Printf("Failed to mount filesystem: %v", err)
+		os.Exit(1)
 	}
 
+	log.Println("RSS filesystem mounted successfully")
 	go handleSignals(server)
 	server.Wait()
 }
@@ -52,20 +66,21 @@ func handleSignals(server *fuse.Server) {
 func replaceHome(path string) string {
 	home, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatal("unable to get homedir: ", err)
+		log.Printf("unable to get homedir: %v", err)
+		os.Exit(1)
 	}
 	return strings.Replace(path, "~", home, 1)
 }
 
-func loadcfg(path string) rss.SubscriptionConfig {
+func loadcfg(path string) (rss.SubscriptionConfig, error) {
 	simple, err := rss.FromFile(path)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	return rss.ToSubscription(simple)
+	return rss.ToSubscription(simple), nil
 }
 
-func createRep(config rss.SubscriptionConfig) []*rss.Node {
+func createRep(config rss.SubscriptionConfig) ([]*rss.Node, error) {
 	rep := []*rss.Node{}
 	cache := rss.NewCache(replaceHome(HACK_CACHE))
 	content := rss.Content{
@@ -75,14 +90,16 @@ func createRep(config rss.SubscriptionConfig) []*rss.Node {
 	for k, _ := range config {
 		data, err := content.GetFeed(k)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Failed to get feed for %s: %v", k, err)
+			continue // Skip this feed instead of failing entirely
 		}
 		in, err := rss.ToInternal(data)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Failed to parse feed for %s: %v", k, err)
+			continue // Skip this feed instead of failing entirely
 		}
 		in.Name = k
 		rep = append(rep, in)
 	}
-	return rep
+	return rep, nil
 }
